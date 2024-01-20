41c6899339133c3400c1f342a02221c26cfcb8a0
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2024-01-20 23:44:58 +0300

    Initial commit

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://smdhxzvz:9uTPKN_LZgM1e83qKgFE_w5gB9VMh0GQ@chimpanzee.rmq.cloudamqp.com/smdhxzvz");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false );
+
+string message = $"hello world {DateTime.Now}";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue", null,messageBody);
+
+Console.WriteLine("Messaj Gönderilmiştir");
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Loader;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://smdhxzvz:9uTPKN_LZgM1e83qKgFE_w5gB9VMh0GQ@chimpanzee.rmq.cloudamqp.com/smdhxzvz");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true,consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Gelen Message : {message}");
+};
+
+Console.ReadLine();
