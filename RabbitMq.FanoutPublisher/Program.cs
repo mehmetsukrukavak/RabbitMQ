16bbeb84d02abff5dfb9104444c843627de51483
Mehmet Şükrü Kavak <mehmet.kavak@dgpaysit.com>
2024-01-21 22:57:17 +0300

    Fanout Exchange

---
+++ RabbitMq.FanoutPublisher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-fanout",durable:true, type:ExchangeType.Fanout);
+
+Enumerable.Range(1,50).ToList().ForEach(x =>
+{
+    string message = $"Message : {x}, {DateTime.Now}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("logs-fanout","", null,messageBody);
+
+    Console.WriteLine($"Messaj Gönderilmiştir: {message}");
+});
+
+Console.ReadLine();
---
+++ RabbitMq.FanoutSubscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Loader;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var randomQueueName = channel.QueueDeclare().QueueName;
+//Mesajları kalıcı hale getirip servis durup başladığında mesajları tekrar almak için
+//channel.QueueDeclare("queue-name", true, false, false);
+channel.QueueBind(randomQueueName,"logs-fanout", "", null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(randomQueueName, true,consumer);
+
+Console.WriteLine("Loglar Dinleniyor...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1500);
+    Console.WriteLine($"Gelen Message : {message}");
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
