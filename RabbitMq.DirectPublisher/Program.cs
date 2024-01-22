57f9fe95ee979a50b7202a0ddc659245fb56a890
Mehmet Şükrü Kavak <mehmet.kavak@dgpaysit.com>
2024-01-22 11:20:19 +0300

    Direct Exchange

---
+++ RabbitMq.DirectPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMq.DirextPublisher;
+
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+var exchangeName = "logs-direct";
+channel.ExchangeDeclare(exchangeName, durable: true, type: ExchangeType.Direct);
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, exchangeName, routeKey,null);
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    LogNames log = (LogNames)new Random().Next(1, 5);
+    string message = $"Log : {x}, log-type : {log},  {DateTime.Now}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    var routeKey = $"route-{log}";
+    channel.BasicPublish(exchangeName, routeKey, null, messageBody);
+
+    Console.WriteLine($"Log Gönderilmiştir: {message}");
+});
+
+Console.ReadLine();
--- RabbitMq.DirectSubscriber/Program.cs
+++ RabbitMq.DirectSubscriber/Program.cs
@@ -9,23 +9,20 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-var randomQueueName = channel.QueueDeclare().QueueName;
-//Mesajları kalıcı hale getirip servis durup başladığında mesajları tekrar almak için
-//channel.QueueDeclare("queue-name", true, false, false);
-channel.QueueBind(randomQueueName,"logs-fanout", "", null);
+
 channel.BasicQos(0, 1, false);
 
 var consumer = new EventingBasicConsumer(channel);
-
-channel.BasicConsume(randomQueueName, true,consumer);
+var queueName = "direct-queue-Error";
+channel.BasicConsume(queueName, true,consumer);
 
 Console.WriteLine("Loglar Dinleniyor...");
 
 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
-    //Thread.Sleep(1500);
-    Console.WriteLine($"Gelen Message : {message}");
+    Thread.Sleep(1500);
+    Console.WriteLine($"Gelen Log : {message}");
     channel.BasicAck(e.DeliveryTag, false);
 };
 
